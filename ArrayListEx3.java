544a1098e004b25e649aba1f2a1e8aa885004a0d
skandhwa <113922787+skandhwa@users.noreply.github.com>
2024-12-28 21:16:20 +0530

    commiting my changes

---
+++ ArrayListEx3.java
@@ -0,0 +1,24 @@
+package CollectionsEx;
+
+import java.util.ArrayList;
+
+public class ArrayListEx3 {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Object> li=new ArrayList<Object>();
+		li.add("Orange");
+		li.add(23);
+		li.add(true);
+		li.add(5600.5f);
+		
+		for(Object x:li)
+		{
+			System.out.println(x);
+		}
+		
+		
+
+	}
+
+}
